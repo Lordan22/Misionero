4c085ad98b64d8e021273fc2ba80223ee97674d3
lordan22 <miguelarias0422@gmail.com>
2024-01-18 07:09:22 -0500

    Se realizo un cambio en el documento de edad, se añadio una d al print de cuando el usuario es menor de edad

---
+++ edad.py
@@ -0,0 +1,7 @@
+nombre= input("Ingrese por favor su nombre")
+edad= int(input("Ingrese por favor su edad"))
+
+if edad >=18:
+    print(f"(nombre)eres mayor de edad")
+    else
+    print(f"(nombre)eres menor de edadd")
